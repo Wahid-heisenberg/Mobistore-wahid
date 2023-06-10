a06eec3bb0b34895164bdf9f95e34539dd4cf847
wahid_slumani <wahidslimani20002@gmail.com>
2023-06-10 17:17:07 +0200

    no

--- src/database/db.js
+++ src/database/db.js
@@ -3,43 +3,7 @@ const path = require('path');
 // const fs = require('fs');
 const sqlite3 = require('sqlite3').verbose();
 
-// const credentials = {
-//   client_id: '668281683453-7r4r69tlaqfebkgmdpnslkq98abv824f.apps.googleusercontent.com',
-//   client_secret: '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',
-//   auth_uri: "https://accounts.google.com/o/oauth2/auth",
-//   token_uri: "https://oauth2.googleapis.com/token",
-// };
-// // Create a new instance of the Google Drive API client
-// const authClient = new google.auth.OAuth2(credentials.client_id, credentials.client_secret);
-// authClient.setCredentials({
-//   access_token: credentials.access_token,
-//   refresh_token: credentials.refresh_token,
-// });
 
-// const fileId = '1xphOYqFAr6I-hw2e9b11D4CVmZnmx2pL';
-// const databaseFilePath =  path.resolve(__dirname, '../../public/database.db');
-
-// // Download the SQLite database file from Google Drive
-// async function downloadDatabaseFile() {
-//   const drive = google.drive({ version: 'v3', auth: authClient });
-//   const dest = fs.createWriteStream(databaseFilePath);
-
-//   await drive.files.get({ fileId: fileId, alt: 'media' }, { responseType: 'stream' })
-//     .then(res => {
-//       res.data
-//         .on('end', () => {
-//           console.log('Download completed.');
-//           accessDatabase();
-//         })
-//         .on('error', err => {
-//           console.error('Error during download.', err);
-//         })
-//         .pipe(dest);
-//     })
-//     .catch(err => {
-//       console.error('Error retrieving file from Google Drive.', err);
-//     });
-// }
 const mysql = require('mysql2');
 const dotenv = require('dotenv').config ({path:__dirname + '/.env'})
 // console.log(process.env.DB_HOST)
